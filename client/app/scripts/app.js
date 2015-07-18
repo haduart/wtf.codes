e51881000e902d1bb1d741c77934762f0ada89cd
Daniil <daniil@redmart.com>
2015-07-18 22:05:12 +0800

    User can see list of codes with hardcoded languages

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -1,18 +1,36 @@
 'use strict';
 angular.module('app', ['ui.router',
   'ui.bootstrap',
-  'ngMaterial'
-]).config(function ($stateProvider, $httpProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/');
+  'ngMaterial',
+  'restangular'
+]).config(function($stateProvider, $httpProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/language/all');
 
   $stateProvider.state('app', {
-    url: '/:language',
-    views: {
-      'main': {
-        templateUrl: 'views/main.html',
-        controller: 'mainController'
+    abstract: true,
+    url: '/',
+    views: {}
+  })
+    .state('app.language', {
+      url: 'language/:language',
+      views: {
+        'main@': {
+          templateUrl: 'views/main.html',
+          controller: 'mainController'
+        },
+        'menu@': {
+          templateUrl: 'views/menu.html',
+          controller: 'menuController'
+        },
+        'header@': {
+          templateUrl: 'views/header.html',
+          controller: 'headerController'
+        }
       }
-    }
-  });
+    });
+}).run(function($rootScope, $state) {
+  //Restangular.setBaseUrl('http://mpapi.alpha.redmart.com/v1.0/');
+  $rootScope.state = $state;
 });
+
 
